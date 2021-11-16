639638b7bce29272fff214457aaf81ef6947fbac
Ghazal J <ghazal.jain.97@gmail.com>
2021-11-16 14:32:25 -0800

    Initial commit

---
+++ Assets/Scripts/CustomRenderPiplelineAsset.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Asset")]
+public class CustomRenderPiplelineAsset : RenderPipelineAsset
+{
+
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new CustomRenderPipeline();
+    }
+}
